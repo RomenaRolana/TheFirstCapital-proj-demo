997d1d592abb90581599e28a34188d1adbb4779a
VladRosolov <rosolovvlad@gmail.com>
2022-02-21 09:41:48 +0200

    add modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -26,3 +26,27 @@
     modal.classList.toggle('is-hidden');
   }
 })();
+
+
+  // Modal for about
+(() => {
+
+  const refs = {
+    openmodalBtn: document.querySelector('[data-modal-open]'),
+    closemodalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+
+  };
+
+
+  refs.openmodalBtn.addEventListener('click', e => {
+    toggleContacts(refs.modal);
+  });
+  refs.closemodalBtn.addEventListener('click', e => {
+    toggleContacts(refs.modal);
+  });
+
+  function toggleContacts(modal) {
+    modal.classList.toggle('is-hidden');
+  }
+})();
